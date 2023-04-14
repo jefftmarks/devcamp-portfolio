60477fa0a38d4b40f10a753ddf6fc382b3f57993
Jeff Marks <jeff.t.marks@gmail.com>
2023-04-14 10:15:55 -0400

    Integrated skills via model generator

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,4 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
