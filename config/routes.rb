637ebef96a91735dc2bf06a73bc3b6e41de238e5
Jeff Marks <jeff.t.marks@gmail.com>
2023-04-17 10:31:05 -0400

    Configured routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
   resources :blogs
+
+  root to: 'pages#home'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end
